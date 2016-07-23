ce3805cb30d4a541c703bc08e108bd9bd4d7883b
AhwinMulky <ashwinmulky@gmail.com>
2016-07-23 15:06:21 +0530

    adding new project

---
+++ app/Http/Controllers/LinkedInAuthenticationController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\User;
+
+use Socialite;
+
+class LinkedInAuthenticationController extends Controller
+{
+     /**
+     * Redirect the user to the Linkedin authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider()
+    {
+        /*return Socialite::driver('linkedin')->scopes(['r_basicprofile', 'r_emailaddress', 'rw_company_admin'])->redirect();*/
+
+        return Socialite::driver('linkedin')->redirect();
+    }
+
+    /**
+     * Obtain the user information from Linkedin.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback()
+    {
+        
+
+        try {
+           $user = Socialite::driver('linkedin')->user();
+        } catch (Exception $e) {
+            return view('error');
+        }
+
+        // $user->token;
+
+        $authUser = $this->findOrCreateUser($user);
+
+       $token = \JWTAuth::fromUser($user);
+
+       return response()->json(compact('token'));
+
+        
+    }
+
+     /**
+     * Return user if exists; create and return if doesn't
+     *
+     * @param $linkedinUser
+     * @return User
+     */
+    private function findOrCreateUser($linkedinUser)
+    {
+        if ($authUser = User::where('linkedin_id', $linkedinUser->id)->first()) {
+            return $authUser;
+        }
+
+        return User::create([
+            'linkedin_id' => $linkedinUser->getId(),
+            'name' => $linkedinUser->getName(),
+            'email' => $linkedinUser->getEmail(),
+        ]);
+    }
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'api'], function(){
+	
+	Route::post('authenticate','JWTAuthenticationController@authenticate');
+	Route::post('signup', 'JWTAuthenticationController@signup');
+
+	Route::get('me', 'UserController@index');
+
+
+	Route::get('auth/linkedin', 'LinkedInAuthenticationController@redirectToProvider');
+	Route::get('auth/linkedin/callback', 'LinkedInAuthenticationController@handleProviderCallback');
+	
+});
